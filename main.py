4377d2731e75f6327913d15d1a2cd38560e939b3
alxjsndc <126742649+alxjsndc@users.noreply.github.com>
2023-03-20 06:39:56 +0000

    Lab12 3-1

--- main.py
+++ main.py
@@ -2,23 +2,27 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    email = input('Enter your string')
-
-    flag = True
-    if not email[0].isalpha():
-        flag = False
-    lenemail = len(email)
-    if lenemail <= 5 or lenemail >= 30:
-        flag = False
-    if email.find('@') == -1:
-        flag = False
-    else:
-        atidx = email.find('@')
-    if email[atidx+1:].find('.') == -1:
-        flag = False
-
-    print (flag)
+    pass
 
 
+import random
+
+# Generate 3 random numbers
+number1 = random.randint(0,100)
+number2 = random.randint(0,100)
+number3 = random.randint(0,100)
+print(number1, number2, number3)
+
+# Check if number1 is the smallest variable
+if number1 < number2 and number1 < number3:
+    print(number1)
+
+# If number1 isn't the smallest, check if number2 is
+elif number2 < number1 and number2 < number3:
+    print(number2)
+
+# otherwise, number3 is the smallest.
+else:
+    print(number3)
 if __name__ == '__main__':
     main()
